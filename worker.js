417a7195070c6e0a39377281701f29cbc880e601
kotkotenok45 <itzrealsaddude@gmail.com>
2026-07-10 17:46:30 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,29 @@
-export default {
+export class SignalingRoom {
+  constructor(state) { this.state = state; this.sessions = new Set(); }
+
   async fetch(request) {
-    if (request.headers.get("Upgrade") === "websocket") {
-      const pair = new WebSocketPair();
-      const [client, server] = Object.values(pair);
-      server.accept();
-      
-      server.addEventListener("message", event => {
-        // Просто пересылаем сообщение всем подключенным (простая комната)
-        server.send(event.data); 
-      });
-      return new Response(null, { status: 101, webSocket: client });
-    }
-    return new Response("Сигнальный сервер работает", { status: 200 });
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+    server.accept();
+
+    this.sessions.add(server);
+    server.addEventListener('message', event => {
+      // Рассылаем сигнал всем участникам комнаты (стримеру и зрителям)
+      for (let session of this.sessions) {
+        if (session !== server) session.send(event.data);
+      }
+    });
+
+    server.addEventListener('close', () => this.sessions.delete(server));
+    return new Response(null, { status: 101, webSocket: client });
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const roomId = url.pathname.slice(1) || "default";
+    const id = env.ROOM.idFromName(roomId);
+    return env.ROOM.get(id).fetch(request);
   }
 };
